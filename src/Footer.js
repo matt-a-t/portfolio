e942ee34b7461086297a4387ddf1761cb4456dce
matt armstrong <matt.armstro@gmail.com>
2022-02-05 20:34:21 -1000

    continued refactor

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Icon } from "react-icons-kit";
 import { github, twitter, linkedin } from "react-icons-kit/icomoon";
--- src/Work.js
+++ src/Work.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Parallax } from "react-parallax";
 
 import { yellow, black } from "./colors";
